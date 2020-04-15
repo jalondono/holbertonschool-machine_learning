b8388700d39ab7daf3306c7fdf9e1f9502a69b74
jalondono <juanlondono151776@hotmail.com>
2020-04-15 15:54:38 -0500

    Upload task 4

--- math/0x00-linear_algebra/5-across_the_planes.py
+++ math/0x00-linear_algebra/5-across_the_planes.py
@@ -5,6 +5,7 @@ matrix_shape = __import__('2-size_me_please').matrix_shape
 
 
 def add_matrices2D(mat1, mat2):
+    """adds two 2D matrices"""
     added_matrix = []
     if matrix_shape(mat1) != matrix_shape(mat2):
         return None
