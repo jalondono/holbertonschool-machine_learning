b25491c2556985dcc6466b51f606cd13b9790f30
jalondono <juanlondono151776@hotmail.com>
2020-04-15 19:45:43 -0500

    Upload task 7

--- math/0x00-linear_algebra/7-gettin_cozy.py
+++ math/0x00-linear_algebra/7-gettin_cozy.py
@@ -1,13 +1,22 @@
 #!/usr/bin/env python3
 """ Gettin’ Cozy  """
 
-from copy import deepcopy
+
+def copy2dArray(array):
+    """  Copy a 2dArray:"""
+    copy_array = []
+    for row in array:
+        aux_list = []
+        for column in row:
+            aux_list.append(column)
+        copy_array.append(aux_list)
+    return copy_array
 
 
 def cat_matrices2D(mat1, mat2, axis=0):
     """  that concatenates two matrices along a specific axis:"""
-    aux_mat1 = deepcopy(mat1)
-    aux_mat2 = deepcopy(mat2)
+    aux_mat1 = copy2dArray(mat1)
+    aux_mat2 = copy2dArray(mat2)
     if axis == 0:
         if len(aux_mat1[0]) == len(aux_mat2[0]):
             aux_mat1.extend(aux_mat2)
