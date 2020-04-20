6f63679e76ad02d4643f497f1e2a87731f5874b6
jalondono <juanlondono151776@hotmail.com>
2020-04-20 11:45:13 -0500

    upload task 4

---
+++ math/0x01-plotting/4-frequency.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import numpy as np
+import matplotlib.pyplot as plt
+
+np.random.seed(5)
+student_grades = np.random.normal(68, 15, 50)
+
+plt.xlabel('Grades')
+plt.ylabel('Number of Students')
+plt.title('Project A')
+plt.hist(student_grades, bins=[0, 10, 20, 30, 40, 50, 60, 70, 80, 90, 100])
+plt.ylim([0, 30])
+plt.show()
