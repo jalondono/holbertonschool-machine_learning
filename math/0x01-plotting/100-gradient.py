94b5a4fb41931edb6545a9e293f522c76dae8f27
jalondono <juanlondono151776@hotmail.com>
2020-04-20 18:12:30 -0500

    upload task 100

--- math/0x01-plotting/100-gradient.py
+++ math/0x01-plotting/100-gradient.py
@@ -8,4 +8,11 @@ x = np.random.randn(2000) * 10
 y = np.random.randn(2000) * 10
 z = np.random.rand(2000) + 40 - np.sqrt(np.square(x) + np.square(y))
 
-# your code here
+marker_size = 20
+plt.scatter(x, y, marker_size, c=z)
+plt.title("Mountain Elevation")
+plt.xlabel("x coordinate (m)")
+plt.ylabel("y coordinate (m)")
+cbar = plt.colorbar()
+cbar.set_label("elevation (m)", labelpad=+2)
+plt.show()
