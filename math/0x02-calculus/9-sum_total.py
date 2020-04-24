77d3f6adc613bc9aafad63df5baf4f30dc6e7610
jalondono <juanlondono151776@hotmail.com>
2020-04-24 15:28:26 -0500

    added type condition

--- math/0x02-calculus/9-sum_total.py
+++ math/0x02-calculus/9-sum_total.py
@@ -4,7 +4,7 @@
 
 def summation_i_squared(n):
     """that calculates [sum_{i=1}^{n} i^2] :"""
-    if n is None:
+    if n is None or type(n) != int or n <= 0:
         return None
     result = (n * ((n + 1) * (2 * n + 1))) / 6
     return int(result)
