2d3b40cdbcc906421e4c8aca1235f7316ba2de94
jalondono <juanlondono151776@hotmail.com>
2020-04-24 15:25:48 -0500

    upload task from 10

--- math/0x02-calculus/10-matisse.py
+++ math/0x02-calculus/10-matisse.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
-""". Derive happiness in oneself from a good day's work"""
+"""Derive happiness in oneself from a good day's work"""
 
 
 def poly_derivative(poly):
-    derivate = []
     """ that calculates the derivative of a polynomial:"""
-    if poly is None:
+
+    derivate = []
+    if poly is None or len(poly) == 0:
         return None
+
     if len(poly) == 1:
         return [0]
+
+    for element in poly:
+        if not isinstance(element, (int, float)):
+            return None
+
     for i in range(1, len(poly)):
         derivate.append(i * poly[i])
     return derivate
