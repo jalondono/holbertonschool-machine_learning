c64da2c6443591d862724247232dee7b659a24c7
jalondono <juanlondono151776@hotmail.com>
2020-04-25 18:07:36 -0500

    upload task 17

---
+++ math/0x02-calculus/17-integrate.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Integrate"""
+
+
+def poly_integral(poly, C=0):
+    """that calculates the integral of a polynomial: """
+    integrate_list = []
+
+    if type(poly) != list:
+        return None
+
+    if poly is None or len(poly) == 0:
+        return None
+
+    if not isinstance(C, int):
+        return None
+
+    for element in poly:
+        if not isinstance(element, (int, float)):
+            return None
+    integrate_list.append(C)
+    for idx in range(0, len(poly)):
+        integrate_list.append(poly[idx] / (idx + 1))
+    return integrate_list
