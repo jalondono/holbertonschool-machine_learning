7e253c1e9fc96d6449dba0acf51082b6ffcf7698
jalondono <juanlondono151776@hotmail.com>
2020-05-06 13:00:57 -0500

    Fixed sigmoid function

--- supervised_learning/0x00-binary_classification/2-neuron.py
+++ supervised_learning/0x00-binary_classification/2-neuron.py
@@ -5,7 +5,7 @@ import numpy as np
 
 def sigmoid(Z):
     """Activation function of sigmoid neurone"""
-    sigma = (1.0 / (1.0 - np.exp(-Z)))
+    sigma = (1.0 / (1.0 + np.exp(-Z)))
     return sigma
 
 
