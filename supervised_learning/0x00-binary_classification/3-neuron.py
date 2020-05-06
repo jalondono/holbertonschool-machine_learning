c6119cc6234b7701de9c2bf4b1b1f25b33ffc292
jalondono <juanlondono151776@hotmail.com>
2020-05-06 16:09:17 -0500

    fixed cost ecuation

--- supervised_learning/0x00-binary_classification/3-neuron.py
+++ supervised_learning/0x00-binary_classification/3-neuron.py
@@ -71,6 +71,6 @@ class Neuron:
         """
         m = Y.shape[1]
         cost = -np.sum((Y * np.log(A)) +
-                       ((1.0000001 - Y) *
+                       ((1 - Y) *
                         np.log(1.0000001 - A))) / m
         return cost
