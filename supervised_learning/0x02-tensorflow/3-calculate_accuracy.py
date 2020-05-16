82645001da62086582b54d00e7cb75f745887f87
jalondono <juanlondono151776@hotmail.com>
2020-05-16 17:01:37 -0500

    pep8

--- supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
+++ supervised_learning/0x02-tensorflow/3-calculate_accuracy.py
@@ -10,4 +10,11 @@ def calculate_accuracy(y, y_pred):
     :param y_pred: is a tensor containing the network’s predictions
     :return: a tensor containing the decimal accuracy of the prediction
     """
-    return tf.metrics.accuracy(y, y_pred, name='Mean')[0]
+    predict_ones = tf.argmax(y_pred, 1)
+    label_ones = tf.argmax(y, 1)
+
+    equals = tf.math.equal(predict_ones, label_ones)
+    mean = tf.reduce_mean(tf.cast(equals, tf.float32), axis=0)
+    # ptional way
+    # mean = tf.metrics.accuracy(y, y_pred)[0]
+    return mean
