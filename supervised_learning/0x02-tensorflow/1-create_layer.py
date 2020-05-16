bee80e07996f542acef8b69380cbbb2f678fee79
jalondono <juanlondono151776@hotmail.com>
2020-05-16 11:52:38 -0500

    upload task 1

---
+++ supervised_learning/0x02-tensorflow/1-create_layer.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Layers"""
+import tensorflow as tf
+
+
+def create_layer(prev, n, activation):
+    """
+    Layers
+    :param prev:  is the tensor output of the previous layer
+    :param n: s the number of nodes in the layer to create
+    :param activation:  is the activation function that the layer should use
+    :return:
+    """
+    tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+    layer = tf.layers.dense(
+        inputs=prev,
+        units=n,
+        activation=activation,
+        name='layer')
+    return layer
