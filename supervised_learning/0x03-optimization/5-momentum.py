6ba365de613c5d78d4571ba628f00e30e0fd2553
jalondono <juanlondono151776@hotmail.com>
2020-05-23 20:29:56 -0500

    upload task 5

---
+++ supervised_learning/0x03-optimization/5-momentum.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Momentun"""
+
+
+def update_variables_momentum(alpha, beta1, var, grad, v):
+    """
+    updates a variable using the gradient descent with momentum
+    optimization algorithm:
+    :param alpha: is the learning rate
+    :param beta1: is the momentum weight
+    :param var: is a numpy.ndarray containing the variable to be updated
+    :param grad: is a numpy.ndarray containing the gradient of var
+    :param v: is the previous first moment of var
+    :return: the updated variable and the new moment, respectively
+    """
+    v = v * beta1 + ((1 - beta1) * grad)
+    var = var - (alpha * v)
+    return var, v
