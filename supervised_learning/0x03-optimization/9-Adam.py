90bcd3a02e9d6878e9dc25f7f2e1c088d1215bac
jalondono <juanlondono151776@hotmail.com>
2020-05-24 18:02:01 -0500

    upload task 9

---
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+"""Adam"""
+
+
+def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
+    """ updates a variable in place using the Adam optimization algorithm:"""
+    v = (v * beta1) + ((1 - beta1) * grad)
+    s = (s * beta2) + ((1 - beta2) * (grad ** 2))
+    v_corrected = v / (1 - (beta1 ** t))
+    s_corrected = s / (1 - (beta2 ** t))
+    var = var - ((alpha * v_corrected) / (s_corrected ** (1 / 2) + epsilon))
+    return var, v, s
