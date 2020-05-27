b7be5f312fcdd11c7426102dd143d8f61aa1390f
jalondono <juanlondono151776@hotmail.com>
2020-05-26 20:16:33 -0500

    fixed pep8

--- supervised_learning/0x04-error_analysis/0-create_confusion.py
+++ supervised_learning/0x04-error_analysis/0-create_confusion.py
@@ -11,7 +11,8 @@ def create_confusion_matrix(labels, logits):
     :param logits: is a one-hot numpy.ndarray of shape
     (m, classes) containing the predicted labels
     :return: a confusion numpy.ndarray of shape (classes, classes)
-     with row indices representing the correct labels and column indices representing the predicted labels
+     with row indices representing the correct labels and column
+      indices representing the predicted labels
     """
 
     confusion = np.matmul(labels.T, logits)
