28b591b2820f6d3477bdb64213e1b2d2b2e14e67
jalondono <juanlondono151776@hotmail.com>
2020-05-26 21:11:36 -0500

    changed definition

--- supervised_learning/0x04-error_analysis/3-specificity.py
+++ supervised_learning/0x04-error_analysis/3-specificity.py
@@ -10,8 +10,17 @@ def specificity(confusion):
     :return: a numpy.ndarray of shape (classes,)
     containing the specificity of each class
     """
-    TP = np.diagonal(confusion)
-    TN = np.sum(TP) - TP
+    # TP = np.diagonal(confusion)
+    # TN = np.sum(TP) - TP
+    # FP = np.sum(confusion, axis=0) - TP
+    # speci = TN / (TN + FP)
+    # return speci
+    # Tiene sentido utilizar la diagonal la definicion
+    # de abajo, no tiene sentido
+    TP = np.diag(confusion)
+    FN = np.sum(confusion, axis=1) - TP
     FP = np.sum(confusion, axis=0) - TP
-    speci = TN / (TN + FP)
-    return speci
+    TN = np.sum(confusion) - (FP + FN + TP)
+    SPECIFICITY = TN / (FP + TN)
+
+    return SPECIFICITY
