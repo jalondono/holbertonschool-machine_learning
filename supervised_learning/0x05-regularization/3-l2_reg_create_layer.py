13518ba0f2359ce115c935e71e4c561a79af5b99
jalondono <juanlondono151776@hotmail.com>
2020-05-29 19:28:07 -0500

    upload task 3

--- supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
+++ supervised_learning/0x05-regularization/3-l2_reg_create_layer.py
@@ -9,7 +9,7 @@ def l2_reg_create_layer(prev, n, activation, lambtha):
     :param prev: is a tensor containing the output of the previous layer
     :param n: is the number of nodes the new layer should contain
     :param activation: is the activation function that should be used on the layer
-    :param lambtha: is the L2 regularization parameter
+    :param lambtha: is the L2 regularization paramete
     :return: the output of the new layer
     """
     init_reg = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
