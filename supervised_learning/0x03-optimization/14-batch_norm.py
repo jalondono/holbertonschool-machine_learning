8eaf7bd589e88582fa27d4085583683c3ce7e3c4
jalondono <juanlondono151776@hotmail.com>
2020-05-31 12:39:13 -0500

    uploas task 14

---
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" Batch Normalization Upgraded """
+import numpy as np
+import tensorflow as tf
+
+
+def create_batch_norm_layer(prev, n, activation):
+    """creates a batch normalization layer
+    for a neural network in tensorflow:"""
+
+    init = tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
+
+    layer = tf.layers.Dense(units=n,
+                            activation=None,
+                            kernel_initializer=init,
+                            name='layer')
+    mean, variance = tf.nn.moments(layer(prev), axes=0, keepdims=True)
+    beta = tf.Variable(tf.constant(0.0, shape=[n]),
+                       name='beta', trainable=True)
+    gamma = tf.Variable(tf.constant(1.0, shape=[n]),
+                        name='gamma', trainable=True)
+
+    bn = tf.nn.batch_normalization(layer(prev),
+                                   mean=mean,
+                                   variance=variance,
+                                   offset=beta,
+                                   scale=gamma,
+                                   variance_epsilon=1e-8)
+    return activation(bn)
--- supervised_learning/0x03-optimization/9-Adam.py
+++ supervised_learning/0x03-optimization/9-Adam.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Adam"""
+"""Adam optimization"""
 
 
 def update_variables_Adam(alpha, beta1, beta2, epsilon, var, grad, v, s, t):
