044b580971ba6665b29856ae88abb122cfdd4b5c
jalondono <juanlondono151776@hotmail.com>
2020-05-31 12:48:34 -0500

    sv

--- supervised_learning/0x03-optimization/14-batch_norm.py
+++ supervised_learning/0x03-optimization/14-batch_norm.py
@@ -11,25 +11,18 @@ def create_batch_norm_layer(prev, n, activation):
     initializer = \
         tf.contrib.layers.variance_scaling_initializer(mode="FAN_AVG")
 
-    # dense layer model
     model = tf.layers.Dense(units=n,
                             activation=None,
                             kernel_initializer=initializer,
                             name='layer')
 
-    # normalization parameter calculation
     mean, variance = tf.nn.moments(model(prev), axes=0, keep_dims=True)
 
-    # incorporation of trainable parameters beta and gamma
-    # for scale and offset
     beta = tf.Variable(tf.constant(0.0, shape=[n]),
                        name='beta', trainable=True)
     gamma = tf.Variable(tf.constant(1.0, shape=[n]),
                         name='gamma', trainable=True)
 
-    # Normalization over result after activation (with mean and variance)
-    # and later adjusting with beta and gamma for
-    # offset and scale respectively
     adjusted = tf.nn.batch_normalization(model(prev), mean, variance,
                                          offset=beta, scale=gamma,
                                          variance_epsilon=1e-8)
