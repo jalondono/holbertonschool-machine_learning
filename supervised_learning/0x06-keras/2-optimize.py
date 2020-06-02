894ca8abafee801e2e180c7c909e8ba40d010f2b
jalondono <juanlondono151776@hotmail.com>
2020-06-02 10:09:03 -0500

    Added return None

--- supervised_learning/0x06-keras/2-optimize.py
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -19,3 +19,4 @@ def optimize_model(network, alpha, beta1, beta2):
     network.compile(optimizer=opt,
                     loss='categorical_crossentropy',
                     metrics=['accuracy'])
+    return None
