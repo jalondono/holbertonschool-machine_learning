8a999940ab9c4b480b2e15a74b5f4a08b0b69052
jalondono <juanlondono151776@hotmail.com>
2020-06-02 11:46:19 -0500

    upload task 6

---
+++ supervised_learning/0x06-keras/6-train.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python3
+""" Train keras"""
+import tensorflow.keras as K
+
+
+def train_model(network, data, labels, batch_size,
+                epochs, validation_data=None,
+                early_stopping=False, patience=0,
+                verbose=True, shuffle=False):
+    """
+    trains a model using mini-batch gradient descent:
+    :param patience:
+    :param early_stopping:
+    :param validation_data:
+    :param network: is the model to train
+    :param data: is a numpy.ndarray of shape (m, nx)
+     containing the input data
+    :param labels: is a one-hot numpy.ndarray of shape (m, classes)
+     containing the labels of data
+    :param batch_size: is the size of the batch used
+     for mini-batch gradient descent
+    :param epochs: is the number of passes through
+     data for mini-batch gradient descent
+    :param verbose: is a boolean that determines
+     if output should be printed during training
+    :param shuffle: is a boolean that determines
+    whether to shuffle the batches every epoch.
+     Normally, it is a
+    :return: the History object generated after training the model
+    """
+    cb = []
+    if validation_data and early_stopping:
+        cb.append(K.callbacks.EarlyStopping(monitor='val_loss',
+                                            mode='min',
+                                            patience=patience))
+    history = network.fit(x=data,
+                          y=labels,
+                          batch_size=batch_size,
+                          epochs=epochs,
+                          validation_data=validation_data,
+                          verbose=verbose,
+                          shuffle=shuffle,
+                          callbacks=cb)
+    return history
