4ed0a2383a78866a6c8edd8e5c47a8d803c0376b
jalondono <juanlondono151776@hotmail.com>
2020-06-01 19:45:01 -0500

    upload task 2

---
+++ supervised_learning/0x06-keras/2-optimize.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python3
+""" Optimize keras"""
+import tensorflow.keras as K
+
+
+def optimize_model(network, alpha, beta1, beta2):
+    """
+    sets up Adam optimization for a keras model with categorical
+     crossentropy loss and accuracy metrics:
+    :param network:
+    :param alpha:
+    :param beta1:
+    :param beta2:
+    :return:
+    """
+    opt = K.optimizers.Adam(learning_rate=alpha,
+                            beta_1=beta1,
+                            beta_2=beta2)
+    network.compile(optimizer=opt,
+                    loss='categorical_crossentropy',
+                    metrics=['accuracy'])
