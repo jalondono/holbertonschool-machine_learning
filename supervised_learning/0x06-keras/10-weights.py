89a704911d5089a92d8e088c540c822c08835ac5
jalondono <juanlondono151776@hotmail.com>
2020-06-02 19:53:29 -0500

    upload task 10

---
+++ supervised_learning/0x06-keras/10-weights.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+"""  Save and Load Weights  """
+import tensorflow.keras as K
+
+
+def save_weights(network, filename, save_format='h5'):
+    """
+    saves a model’s weights:
+    :param network:
+    :param filename:
+    :param save_format:
+    :return:
+    """
+    network.save_weights(filepath=filename,
+                         save_format=save_format)
+    return None
+
+
+def load_weights(network, filename):
+    """
+    loads a model’s weights:
+    :param network:
+    :param filename:
+    :return:
+    """
+    network.load_weights(filename)
+    return None
