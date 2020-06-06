b6b6af253ed59c544073f7dd74a0db35e94175d4
jalondono <juanlondono151776@hotmail.com>
2020-06-05 20:05:06 -0500

    upload task 4

---
+++ math/0x04-convolutions_and_pooling/4-convolve_channels.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python3
+"""  Convolution with Channels """
+
+import numpy as np
+
+
+def convolve_channels(images, kernel, padding='same', stride=(1, 1)):
+    """
+    performs a convolution on grayscale images:
+    :param images: is a numpy.ndarray with shape (m, h, w)
+     containing multiple grayscale images
+    :param kernel: is a numpy.ndarray with shape (kh, kw)
+     containing the kernel for the convolution
+    :param padding: is either a tuple of (ph, pw), ‘same’, or ‘valid’
+    :param stride: is a tuple of (sh, sw)
+    :return: a numpy.ndarray containing the convolved images
+    """
+    # image sizes
+    m = images.shape[0]
+    h = images.shape[1]
+    w = images.shape[2]
+
+    # Kernel Size
+    kh = kernel.shape[0]
+    kw = kernel.shape[1]
+
+    # Strides
+    sh = stride[0]
+    sw = stride[1]
+
+    # padding
+    ph = 0
+    pw = 0
+
+    if padding == 'same':
+        # padding of zeros
+        ph = int(((h - 1) * sh + kh - h) / 2) + 1
+        pw = int(((w - 1) * sw + kw - w) / 2) + 1
+
+    if isinstance(padding, tuple):
+        ph = padding[0]
+        pw = padding[1]
+
+    out_h = int(((h - kh + (2 * ph)) / sh) + 1)
+    out_w = int(((w - kw + (2 * pw)) / sw) + 1)
+
+    img_padded = np.pad(images,
+                        pad_width=((0, 0), (ph, ph), (pw, pw), (0, 0)),
+                        mode='constant', constant_values=0)
+
+    out_img = np.zeros((m, out_h, out_w))
+    for x in range(out_w):
+        for y in range(out_h):
+            img = img_padded[:, y * sh: y * sh + kh, x * sw: x * sw + kw]
+            pixel = np.sum(img * kernel, axis=(1, 2, 3))
+            out_img[:, y, x] = pixel
+    return out_img
