e8c99755f0be20bd297dc13f78a13b742e2fc0b9
jalondono <juanlondono151776@hotmail.com>
2020-06-12 20:24:03 -0500

    upload task 5

--- supervised_learning/0x07-cnn/5-lenet5.py
+++ supervised_learning/0x07-cnn/5-lenet5.py
@@ -12,3 +12,47 @@ def lenet5(X):
     :return: a K.Model compiled to use Adam optimization
      (with default hyperparameters) and accuracy metrics
     """
+    kernel_init = K.initializers.he_normal(seed=None)
+    shape = X.shape
+
+    model = K.Sequential()
+    model.add(K.layers.Conv2D(filters=6,
+                              kernel_size=(5, 5),
+                              padding='same',
+                              activation='relu',
+                              kernel_initializer=kernel_init,
+                              input_shape=shape[1:]))
+
+    model.add(K.layers.MaxPool2D(input_shape=(2, 2),
+                                 strides=(2, 2)))
+
+    model.add(K.layers.Conv2D(filters=16,
+                              kernel_size=(5, 5),
+                              padding='same',
+                              activation='relu',
+                              kernel_initializer=kernel_init))
+
+    model.add(K.layers.MaxPool2D(input_shape=(2, 2),
+                                 strides=(2, 2)))
+
+    model.add(K.layers.Flatten())
+
+    model.add(K.layers.Dense(units=120,
+                             activation='relu',
+                             kernel_initializer=kernel_init,
+                             ))
+
+    model.add(K.layers.Dense(units=84,
+                             activation='relu',
+                             kernel_initializer=kernel_init,
+                             ))
+
+    model.add(K.layers.Dense(units=10,
+                             kernel_initializer=kernel_init,
+                             activation='softmax'
+                             ))
+
+    model.compile(optimizer=K.optimizers.Adam(),
+                  loss='categorical_crossentropy',
+                  metrics=['accuracy'])
+    return model
