e80983bf36b063570ddbaf745d87232c7b44b536
jalondono <juanlondono151776@hotmail.com>
2020-07-24 17:19:13 -0500

    [Fixed] Issue with matrix of rank two

--- math/0x05-advanced_linear_algebra/1-minor.py
+++ math/0x05-advanced_linear_algebra/1-minor.py
@@ -21,6 +21,35 @@ def check_shape(matrix):
     return 0
 
 
+def determinant(matrix):
+    """
+     that calculates the determinant of a matrix:
+    :param matrix:
+    :return:
+    """
+    if check_shape(matrix) == 3:
+        return 1
+    if check_shape(matrix) == 2:
+        raise TypeError("matrix must be a list of lists")
+    if check_shape(matrix) == 1:
+        raise ValueError("matrix must be a square matrix")
+
+    if len(matrix) == 1:
+        return matrix[0][0]
+    if len(matrix) == 2:
+        a = matrix[0][0]
+        b = matrix[0][1]
+        c = matrix[1][0]
+        d = matrix[1][1]
+        return (a * d) - (b * c)
+    det = 0
+    for idx, data in enumerate(matrix[0]):
+        rows = [row for row in matrix[1:]]
+        n_m = [[val for i, val in enumerate(row) if i != idx] for row in rows]
+        det += data * (-1) ** idx * determinant(n_m)
+    return det
+
+
 def minor(matrix):
     """
     that calculates the minor matrix of a matrix:
@@ -35,22 +64,16 @@ def minor(matrix):
         raise ValueError("matrix must be a non-empty square matrix")
 
     if len(matrix) == 1:
-        return 1
-    if len(matrix) == 2:
-        a = matrix[0][0]
-        b = matrix[0][1]
-        c = matrix[1][0]
-        d = matrix[1][1]
-        return (a * d) - (b * c)
+        return [[1]]
 
     final_mdet = []
+    det = 0
     for index, each_row in enumerate(matrix):
         out_det = []
         rows = matrix[:index] + matrix[index + 1:]
         for idx, data in enumerate(each_row):
-            det = 0
             n_m = [[v for i, v in enumerate(row) if i != idx] for row in rows]
-            det += minor(n_m)
+            det = determinant(n_m)
             out_det.append(det)
         final_mdet.append(out_det)
     return final_mdet
