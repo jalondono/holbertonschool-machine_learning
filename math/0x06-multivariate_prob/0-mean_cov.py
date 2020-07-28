0f90621d856e58a81736e1cff4be94b79e6c6322
jalondono <juanlondono151776@hotmail.com>
2020-07-27 20:48:52 -0500

    [Upload] task 0

---
+++ math/0x06-multivariate_prob/0-mean_cov.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+"""Mean and Covariance """
+import numpy as np
+
+
+def mean_cov(X):
+    """
+    calculates the mean and covariance of a data set:
+    :param X:
+    :return:
+    """
+    if len(X.shape) != 2:
+        raise TypeError(' must be a 2D numpy.ndarray')
+    if X.shape[0] < 2:
+        raise ValueError('X must contain multiple data points')
+    m = X.shape[0]
+    mean = np.sum(X, axis=0) / m
+    mean = mean[np.newaxis, :]
+
+    dev = X - mean
+    cov = np.matmul(dev.T, dev) / (m - 1)
+    return mean, cov
