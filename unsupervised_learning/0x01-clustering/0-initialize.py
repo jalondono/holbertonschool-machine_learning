c0db1cbd620b33bd82ac65575a70d674c013bf97
jalondono <juanlondono151776@hotmail.com>
2020-08-03 17:45:23 -0500

    [updated] Pep8 Task 0

--- unsupervised_learning/0x01-clustering/0-initialize.py
+++ unsupervised_learning/0x01-clustering/0-initialize.py
@@ -5,21 +5,21 @@ import numpy as np
 
 
 def initialize(X, k):
-	"""
-	initializes cluster centroids for K-means:
-	:param X:  is a numpy.ndarray of shape (n, d) containing the
-	dataset that will be used for K-means clustering
-	:param k: is a positive integer containing the number of clusters
-	:return:  a numpy.ndarray of shape (k, d) containing the initialized
-	centroids for each cluster, or None on failure
-	"""
-	if not isinstance(X, np.array) or len(X.shape) != 2:
-		return None
-	if not isinstance(k, int) or k <= 0:
-		return None
+    """
+    initializes cluster centroids for K-means:
+    :param X:  is a numpy.ndarray of shape (n, d) containing the
+    dataset that will be used for K-means clustering
+    :param k: is a positive integer containing the number of clusters
+    :return:  a numpy.ndarray of shape (k, d) containing the initialized
+    centroids for each cluster, or None on failure
+    """
+    if not isinstance(X, np.array) or len(X.shape) != 2:
+        return None
+    if not isinstance(k, int) or k <= 0:
+        return None
 
-	min = np.amin(X, axis=0)
-	max = np.amax(X, axis=0)
-	d = X.shape[1]
-	centroids = np.random.uniform(min, max, (k, d))
-	return centroids
+    min = np.amin(X, axis=0)
+    max = np.amax(X, axis=0)
+    d = X.shape[1]
+    centroids = np.random.uniform(min, max, (k, d))
+    return centroids
