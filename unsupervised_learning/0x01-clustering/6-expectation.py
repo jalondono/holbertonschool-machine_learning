7ca121a6c30298edb0fc4378b986c2d0295d83b1
jalondono <juanlondono151776@hotmail.com>
2020-08-06 16:59:58 -0500

    [upload] task 6

---
+++ unsupervised_learning/0x01-clustering/6-expectation.py
@@ -0,0 +1,34 @@
+#!/usr/bin/env python3
+""" Initialize PDF """
+
+import numpy as np
+pdf = __import__('5-pdf').pdf
+
+
+def expectation(X, pi, m, S):
+    """
+    calculates the expectation step in the EM algorithm for a GMM:
+    :param X: is a numpy.ndarray of shape (n, d) containing the data set
+    :param pi: is a numpy.ndarray of shape (k,) containing the priors for
+    each cluster
+    :param m: is a numpy.ndarray of shape (k, d) containing the centroid
+     means for each cluster
+    :param S: is a numpy.ndarray of shape (k, d, d) containing the covariance
+     matrices for each cluster
+    :return: g, l, or None, None on failure
+    * g is a numpy.ndarray of shape (k, n) containing the posterior
+    probabilities
+    for each data point in each cluster
+    * l is the total log likelihood
+    """
+
+    n, d = X.shape
+    k = pi.shape[0]
+    posterior = np.zeros((k, n))
+
+    for idx in range(k):
+        posterior[idx] = pi[idx] * pdf(X, m[idx], S[idx])
+    marginal = np.sum(posterior, axis=0)
+    posterior = posterior/marginal
+    log_likelihood = np.sum(np.log(marginal))
+    return posterior, log_likelihood
