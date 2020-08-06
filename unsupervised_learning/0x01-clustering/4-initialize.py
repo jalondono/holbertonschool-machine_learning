98cbf95429234680780a5a5ac2bb019b1a32d163
jalondono <juanlondono151776@hotmail.com>
2020-08-05 19:27:29 -0500

    [upload] task 4

---
+++ unsupervised_learning/0x01-clustering/4-initialize.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+""" Initialize GMM """
+
+import numpy as np
+kmeans = __import__('1-kmeans').kmeans
+
+
+def initialize(X, k):
+    """
+     initializes variables for a Gaussian Mixture Model:
+    :param X: is a numpy.ndarray of shape (n, d) containing the data set
+    :param k: is a positive integer containing the number of clusters
+    :return: pi, m, S, or None, None, None on failure
+    """
+    if not isinstance(k, int) or k <= 0 or k >= X.shape[0]:
+        return None, None, None
+    if len(X.shape) != 2 or not isinstance(X, np.ndarray):
+        return None, None, None
+
+    n, d = X.shape
+    pi = np.tile(1/k, (k,))
+    m, _ = kmeans(X, k)
+    identity = np.identity(d)
+    S = np.tile(identity, (k, 1, 1))
+
+    return pi, m, S
