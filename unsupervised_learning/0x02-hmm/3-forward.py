450a0522f46e6a1cf85a002fa607c7c390a34930
jalondono <juanlondono151776@hotmail.com>
2020-08-14 16:58:26 -0500

    [Upload] task 3

---
+++ unsupervised_learning/0x02-hmm/3-forward.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""The Forward Algorithm"""
+import numpy as np
+
+
+def forward(Observation, Emission, Transition, Initial):
+    """
+
+    :param Observation:
+    :param Emission:
+    :param Transition:
+    :param Initial:
+    :return:
+    """
+    T = Observation.shape[0]
+    N = Emission.shape[0]
+    F = np.zeros((N, T))
+    init_observation = Observation[0]
+    a = Initial.T
+    b = Emission[:, init_observation]
+    F[:, 0] = a * b
+    for t in range(1, T):
+        for j in range(Transition.shape[0]):
+            X = F[:, t - 1]
+            Y = Transition[:, j]
+            Z = Emission[j, Observation[t]]
+            F[j, t] = np.sum(X * Y * Z)
+    P = np.sum(F[:, -1:])
+    return P, F
