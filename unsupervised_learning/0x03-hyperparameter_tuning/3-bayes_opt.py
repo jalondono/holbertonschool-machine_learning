179148b23323888bc745ff27128c3d5642845a86
jalondono <juanlondono151776@hotmail.com>
2020-08-20 09:37:18 -0500

    [update] Added comments to class

--- unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
+++ unsupervised_learning/0x03-hyperparameter_tuning/3-bayes_opt.py
@@ -6,6 +6,7 @@ GP = __import__('2-gp').GaussianProcess
 
 
 class BayesianOptimization:
+    """BayesianOptimization"""
     def __init__(self, f, X_init, Y_init, bounds,
                  ac_samples, l=1, sigma_f=1, xsi=0.01,
                  minimize=True):
