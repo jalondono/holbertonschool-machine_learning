0748d94e0d0838da349b62e63fe7414b0b03df12
jalondono <juanlondono151776@hotmail.com>
2020-09-04 18:12:25 -0500

    [upload] Task 5

---
+++ supervised_learning/0x0D-RNNs/5-bi_forward.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+
+import numpy as np
+
+
+class BidirectionalCell:
+    """ BidirectionalCell class"""
+    def __init__(self, i, h, o):
+        """
+        class constructor
+        :param i: is the dimensionality of the data
+        :param h: is the dimensionality of the hidden states
+        :param o: is the dimensionality of the outputs
+        """
+        self.Whf = np.random.normal(size=(i + h, h))
+        self.Whb = np.random.normal(size=(i + h, h))
+        self.Wy = np.random.normal(size=(i + h + o, o))
+
+        self.bhf = np.zeros((1, h))
+        self.bhb = np.zeros((1, h))
+        self.by = np.zeros((1, o))
+
+    def softmax(self, x):
+        """ softmax function """
+        return np.exp(x) / np.sum(np.exp(x), axis=1, keepdims=True)
+
+    def forward(self, h_prev, x_t):
+        """
+        represents a bidirectional cell of an RNN:
+        :param h_prev: is a numpy.ndarray of shape (m, h)
+         containing the previous hidden state
+        :param x_t: is a numpy.ndarray of shape (m, i)
+        that contains the data input for the cell
+        :return: h_next, the next hidden state
+        """
+        a = np.concatenate((h_prev, x_t), axis=1)
+        h_next = np.tanh(np.matmul(a, self.Whf) + self.bhf)
+        return h_next
