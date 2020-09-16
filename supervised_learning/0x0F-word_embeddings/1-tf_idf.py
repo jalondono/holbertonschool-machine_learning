806bffc5e9b3790ab23f0f78b97bdfb3794829c8
jalondono <juanlondono151776@hotmail.com>
2020-09-15 19:36:00 -0500

    Upload task from 0 to 2

---
+++ supervised_learning/0x0F-word_embeddings/1-tf_idf.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+"""Bag of words"""
+from sklearn.feature_extraction.text import TfidfVectorizer
+
+
+def tf_idf(sentences, vocab=None):
+    """
+    creates a TF-IDF embedding:
+    :param sentences: is a list of sentences to analyze
+    :param vocab: is a list of the vocabulary words to use for the analysis
+    :return: embeddings, features
+    """
+    vectorizer = TfidfVectorizer(vocabulary=vocab)
+    X = vectorizer.fit_transform(sentences)
+    return X.toarray(), vectorizer.get_feature_names()
---
+++ supervised_learning/0x0F-word_embeddings/2-word2vec.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+""" Train Word2Vec """
+from gensim.models import Word2Vec
+from gensim.test.utils import common_texts
+
+
+def word2vec_model(sentences, size=100, min_count=5,
+                   window=5, negative=5, cbow=True,
+                   iterations=5, seed=0, workers=1):
+    """
+    creates and trains a gensim word2vec model:
+    :param sentences: is a list of sentences to be trained on
+    :param size: is the dimensionality of the embedding layer
+    :param min_count: is the minimum number of occurrences of a word
+     for use in training
+    :param window: is the maximum distance between the current
+    and predicted word within a sentence
+    :param negative: is the size of negative sampling
+    :param cbow: is a boolean to determine the training type;
+     True is for CBOW; False is for Skip-gram
+    :param iterations: is the number of iterations to train over
+    :param seed: is the seed for the random number generator
+    :param workers: is the number of worker threads to train the model
+    :return: the trained model
+    """
+    model = Word2Vec(sentences, size=size, window=window,
+                     min_count=min_count, negative=negative,
+                     sg=cbow, seed=seed, iter=iterations)
+    model.train(sentences, total_examples=model.corpus_count,
+                epochs=model.epochs)
+    return model
