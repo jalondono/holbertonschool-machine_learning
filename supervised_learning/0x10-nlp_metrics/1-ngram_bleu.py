aee764af70efc4ab0ad0981f2357c29316105369
jalondono <juanlondono151776@hotmail.com>
2020-09-18 11:28:30 -0500

    upload task 2

--- supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
+++ supervised_learning/0x10-nlp_metrics/1-ngram_bleu.py
@@ -11,7 +11,7 @@ def ngram_bleu(references, sentence, n):
     :return:
     """
     #  initialization of variables
-    win_size = 1
+    win_size = n
     tokenized_sentence = []
     word_instaces = {}
     is_there = False
@@ -21,19 +21,12 @@ def ngram_bleu(references, sentence, n):
     # split the sentence on sublist of windows size
     for idx in range(len(sentence) - win_size + 1):
         tokenized_sentence.append(sentence[idx:win_size + idx])
-    print()
 
     # count the unigrams on sentence
     for token in tokenized_sentence:
         for idx in range(len(sentence) - win_size + 1):
             if token == sentence[idx:idx + win_size]:
                 count += 1
-
-    # min len of references
-    r_list = np.array([np.abs(len(s) - count) for s in references])
-    r_ind = np.argwhere(r_list == np.min(r_list))
-    lens = np.array([len(s) for s in references])[r_ind]
-    r = np.min(lens)
 
     # count the cliping on references
     for ref in references:
@@ -46,9 +39,18 @@ def ngram_bleu(references, sentence, n):
     final_clip = max(clip_list)
     precision = final_clip / count
 
-    if count > r:
+    len_trans = len(sentence)
+
+    # Brevity penalty
+    # closest reference length from translation length
+    closest_ref_idx = np.argmin([abs(len(x) - len_trans) for x in references])
+    reference_length = len(references[closest_ref_idx])
+
+    if len_trans > reference_length:
         BP = 1
     else:
-        BP = np.exp(1 - (r / count))
-        Bleu = BP * precision
-    return Bleu
+        BP = np.exp(1 - float(reference_length) / len_trans)
+
+    bleu = BP * precision
+
+    return bleu
--- supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
+++ supervised_learning/0x10-nlp_metrics/2-cumulative_bleu.py
@@ -3,6 +3,36 @@
 import numpy as np
 
 
+def compute_presicion(references, sentence, n):
+    win_size = n
+    tokenized_sentence = []
+    word_instaces = {}
+    is_there = False
+    count = 0
+    clip_list = []
+
+    # split the sentence on sublist of windows size
+    for idx in range(len(sentence) - win_size + 1):
+        tokenized_sentence.append(sentence[idx:win_size + idx])
+
+    # count the unigrams on sentence
+    for token in tokenized_sentence:
+        for idx in range(len(sentence) - win_size + 1):
+            if token == sentence[idx:idx + win_size]:
+                count += 1
+
+    # count the cliping on references
+    for ref in references:
+        clip = 0
+        for idx in range(len(ref) - win_size + 1):
+            for token in tokenized_sentence:
+                if ref[idx:idx + win_size] == token:
+                    clip += 1
+        clip_list.append(clip)
+    final_clip = max(clip_list)
+    return final_clip / count
+
+
 def cumulative_bleu(references, sentence, n):
     """
     calculates the cumulative n-gram BLEU score for a sentence:
@@ -10,3 +40,24 @@ def cumulative_bleu(references, sentence, n):
     :param sentence: is a list containing the model proposed sentence
     :return:
     """
+    precisions = [0] * n
+    for idx in range(0, n):
+        precisions[idx] = compute_presicion(references, sentence, idx+1)
+
+    geo_mean = np.exp(np.sum(np.log(precisions) / n))
+
+    len_trans = len(sentence)
+
+    # Brevity penalty
+    # closest reference length from translation length
+    closest_ref_idx = np.argmin([abs(len(x) - len_trans) for x in references])
+    reference_length = len(references[closest_ref_idx])
+
+    if len_trans > reference_length:
+        BP = 1
+    else:
+        BP = np.exp(1 - float(reference_length) / len_trans)
+
+    bleu = BP * geo_mean
+
+    return bleu
