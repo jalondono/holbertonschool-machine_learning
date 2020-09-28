cd334936bc25acb0332dc06585cf596f788789f9
jalondono <juanlondono151776@hotmail.com>
2020-09-28 15:47:57 -0500

    [Upload] Task 0

---
+++ reinforcement_learning/0x00-q_learning/0-load_env.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+"""Load the Environment"""
+import gym
+
+
+def load_frozen_lake(desc=None, map_name=None, is_slippery=False):
+    """
+     loads the pre-made FrozenLakeEnv evnironment from OpenAI’s gym:
+    :param desc: is either None or a list of lists containing a custom
+     description of the map to load for the environment
+    :param map_name: is either None or a string containing the pre-made
+     map to load
+    :param is_slippery: is a boolean to determine if the ice is slippery
+    :return: the environment
+    """
+    env = gym.make("FrozenLake-v0", desc=desc, map_name=map_name,
+                   is_slippery=is_slippery)
+    return env
