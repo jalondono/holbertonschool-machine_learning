1aa24a73cf122d575e8ac5797471d44afb46df21
jalondono <juanlondono151776@hotmail.com>
2020-09-29 12:03:34 -0500

    [Upload] Task 4

---
+++ reinforcement_learning/0x00-q_learning/4-play.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+"""contains the play function"""
+
+import numpy as np
+
+
+def play(env, Q, max_steps=100):
+    """
+    has the trained agent play an episode
+    :param env: the FrozenLakeEnv instance
+    :param Q: numpy.ndarray containing the Q-table
+    :param max_steps: maximum number of steps in the episode
+    :return: the total rewards for the episode
+    """
+    state = env.reset()
+    env.render()
+    for step in range(max_steps):
+        # Choose action with highest Q-value for current state
+        action = np.argmax(Q[state, :])
+
+        # perform step (take new action)
+        state, reward, done, info = env.step(action)
+
+        # render current state of environment
+        env.render()
+
+        if done:
+            break
+
+    return reward
